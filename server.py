2ee53e67c03a35108666896118eebc2a3a57a878
passtime <passtime@passtime.tf>
2024-04-03 16:30:08 +0000

    Allows custom port for server.py

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
+import sys
+
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,5 +9,11 @@ def hello():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    port = 5000
+    if len(sys.argv) > 1:
+        try:
+            port = int(sys.argv[1])
+        except ValueError:
+            print("Invalid port number. Using default port 5000.")
+    app.run(host='0.0.0.0', port=port)
 
